3e3a12f014243aa40a0b594aa2012a27dc53d7e3
Sathiya Narayanan <sathiyanarayanan07@gmail.com>
2025-08-02 11:28:57 +0530

    update

--- index.php
+++ index.php
@@ -7,9 +7,18 @@
    $quantity = 4;
    $price =200;
 
+   // boolean  //
+   $online = true;
+   $offline= false;
+
 //total//
 echo" food:{$food},<br>Quantity:{$quantity}<br>";
 $total = $quantity * $price;
-echo"total amount:{$total}";
+echo"total amount:{$total}<br>";
+
+
+// boolean value // 1="true, 0=false
+echo"user in online:{$online}<br>";
+echo "user in offline:{$offline}";
 
 ?>
