66df8f8b443d5f80ba0a924bbd5f20a2a47b41f6
Sathiya Narayanan <sathiyanarayanan07@gmail.com>
2025-08-02 10:59:33 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+   $name = "sathiya";
+   $food = "briyani";
+   $email = "food@gmail.com";
+   $age = 21;
+
+   echo"hello {$name}<br>";
+   echo "order {$food} please<br>";
+   echo "order send to your email: {$email}<br>";
