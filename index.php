1e617c9f666d132d5be4ae5a265e36e70c70b97a
Sathiya Narayanan <sathiyanarayanan07@gmail.com>
2025-08-02 11:23:39 +0530

    update

--- index.php
+++ index.php
@@ -5,7 +5,11 @@
    $age = 21;
    $user = 2;
    $quantity = 4;
+   $price =200;
 
-
+//total//
+echo" food:{$food},<br>Quantity:{$quantity}<br>";
+$total = $quantity * $price;
+echo"total amount:{$total}";
 
 ?>
