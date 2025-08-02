86fd178ab785d1ae0027e67ef5ed6c96b6bb07bc
Sathiya Narayanan <sathiyanarayanan07@gmail.com>
2025-08-02 11:15:11 +0530

    updated logic in index.php

--- index.php
+++ index.php
@@ -3,7 +3,9 @@
    $food = "briyani";
    $email = "food@gmail.com";
    $age = 21;
+   $user = 2;
+   $quantity = 4;
 
-   echo"hello {$name}<br>";
-   echo "order {$food} please<br>";
-   echo "order send to your email: {$email}<br>";
+
+
+?>
